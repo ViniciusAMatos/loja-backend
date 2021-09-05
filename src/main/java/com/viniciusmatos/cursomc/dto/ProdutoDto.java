4f5373ab2cc6b986de4e403ba8df27d0d3a3da50
Vinicius Matos <14viniciusandre@gmail.com>
2021-09-05 19:17:22 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/viniciusmatos/cursomc/dto/ProdutoDto.java
@@ -0,0 +1,46 @@
+package com.viniciusmatos.cursomc.dto;
+
+import com.viniciusmatos.cursomc.domain.Produto;
+
+import java.io.Serializable;
+
+public class ProdutoDto implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String nome;
+    private Double preco;
+
+    public ProdutoDto(){
+
+    }
+
+    public ProdutoDto(Produto obj) {
+        id = obj.getId();
+        nome = obj.getNome();
+        preco = obj.getPreco();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
